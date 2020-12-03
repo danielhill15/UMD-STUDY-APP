462862762a1e6b82968e515a7cc5e4ffdc81a3e3
Daniel <danielhill189@gmail.com>
2020-12-03 00:26:05 -0500

    working_login_register
    firebase authentication

---
+++ login_firebase/app/src/main/java/com/e/studyapplogin/DashboardActivity.kt
@@ -0,0 +1,19 @@
+package com.e.studyapplogin
+
+import android.content.Intent
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class DashboardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
--- login_firebase/app/src/main/java/com/e/studyapplogin/MainActivity.kt
+++ login_firebase/app/src/main/java/com/e/studyapplogin/MainActivity.kt
@@ -3,11 +3,15 @@ package com.e.studyapplogin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.widget.Button
+import android.widget.ImageView
+import androidx.core.content.res.ResourcesCompat
 
 class MainActivity : AppCompatActivity() {
     private var registerBtn: Button? = null
     private var loginBtn: Button? = null
+    private var umdImg: ImageView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,5 +31,6 @@ class MainActivity : AppCompatActivity() {
     private fun initializeViews() {
         registerBtn = findViewById(R.id.register)
         loginBtn = findViewById(R.id.login)
+        umdImg = findViewById(R.id.image)
     }
 }
